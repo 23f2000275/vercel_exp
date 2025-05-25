8cf11df94b95e6a720d0813c3193d574ee605b9a
23f2000275 <23f2000275@ds.study.iitm.ac.in>
2025-05-25 13:18:23 +0000

    seventh commit

--- api/index.py
+++ api/index.py
@@ -27,7 +27,7 @@ def get_marks(name: Optional[List[str]] = Query(None)) -> Dict[str, List[int]]:
     marks = []
     for n in name:
         if n not in marks_data:
-            raise HTTPException(status_code=404, detail=f"Name '{n}' not found")
+            return {"marks": []}
         marks.append(marks_data[n])
     return {"marks": marks}
 
